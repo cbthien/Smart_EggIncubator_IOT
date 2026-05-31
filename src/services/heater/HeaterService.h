44f853245a7b15ee5786b0057e6dab8f7bdb0fbd
CaoThien <caothien0502@gmail.com>
2026-05-31 23:01:52 +0700

    feat: dual heater control with hysteresis and anti-chatter protection

--- src/services/heater/HeaterService.h
+++ src/services/heater/HeaterService.h
@@ -1,3 +1,9 @@
+/******************************************************************
+ * Smart Egg Incubator - Heater Service
+ * Controls 2 heaters (left/right) via relay with hysteresis
+ * Anti-chatter: minimum ON/OFF time to protect relay
+ ******************************************************************/
+
 #ifndef HEATER_SERVICE_H
 #define HEATER_SERVICE_H
 
@@ -12,11 +18,18 @@ public:
     void begin(AppContext* context);
     void loop();
 
-    void setHeater(bool state);
+    void setHeaters(bool state);
     void forceOff();
+
+    bool isOn() const;
 
 private:
     AppContext* ctx;
+    unsigned long lastStateChangeMs;
+    bool currentState;
+
+    static const unsigned long MIN_ON_TIME_MS = 30000;   // 30s minimum ON
+    static const unsigned long MIN_OFF_TIME_MS = 10000;  // 10s minimum OFF
 };
 
 #endif
